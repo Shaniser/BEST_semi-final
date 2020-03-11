3ae3b796ce7fe5eac933cd30b987e972e1ec6b32
Shaniser <mr.shaniser@gmail.com>
2020-03-11 13:53:16 +0300

    изменен тулбар

--- app/src/main/java/com/godelsoft/bestsemi_final/MainActivity.kt
+++ app/src/main/java/com/godelsoft/bestsemi_final/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import org.jetbrains.anko.startActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.event_card.*
 import org.jetbrains.anko.startActivityForResult
 
 
@@ -29,24 +31,16 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_chat))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.action_bar, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.itemFilter -> startActivity<PeopleActivity>()
-            R.id.itemSettings -> startActivity<MyAccountActivity>()
+        findViewById<View>(R.id.container).apply {
+            filter.setOnClickListener {
+                startActivity<PeopleActivity>()
+            }
+            account.setOnClickListener{
+                startActivity<MyAccountActivity>()
+            }
         }
-        return true
     }
 
     fun showFAB() {
--- app/src/main/java/com/godelsoft/bestsemi_final/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/godelsoft/bestsemi_final/ui/dashboard/DashboardFragment.kt
@@ -23,10 +23,7 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        
         if (activity is MainActivity) {
             (activity as MainActivity).hideFAB()
         }
