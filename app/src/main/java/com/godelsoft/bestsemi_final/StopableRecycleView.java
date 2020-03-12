e22c3971347ae94935c94eb16791c8a66426c3e8
Shaniser <mr.shaniser@gmail.com>
2020-03-12 16:14:55 +0300

    popUp

---
+++ app/src/main/java/com/godelsoft/bestsemi_final/StopableRecycleView.java
@@ -0,0 +1,37 @@
+package com.godelsoft.bestsemi_final;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StopableRecycleView extends RecyclerView {
+    private boolean scrollEnable = true;
+    public StopableRecycleView(@NonNull Context context) {
+        super(context);
+    }
+
+    public StopableRecycleView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StopableRecycleView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean canScrollVertically(int direction) {
+        return scrollEnable && super.canScrollVertically(direction);
+    }
+
+    @Override
+    public boolean canScrollHorizontally(int direction) {
+        return scrollEnable && super.canScrollHorizontally(direction);
+    }
+
+    public void setScrollEnable(boolean scrollEnable) {
+        this.scrollEnable = scrollEnable;
+    }
+}
